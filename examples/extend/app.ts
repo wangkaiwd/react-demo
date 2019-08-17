a693e3eb5aaf78e380aa1884679f8ed85119d51a
wangkaiwd <1162486176@qq.com>
2019-08-17 21:23:59 +0800

    feat: create extend test case for extend/app.ts

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,5 +1,33 @@
 import axios from '../../src'
 
 axios({
-
+  url: '/extend/get',
+  params: {
+    a: 1,
+    b: 2
+  },
+  method: 'get'
 })
+
+axios.request({
+  url: '/extend/get',
+  params: {
+    a: 1,
+    b: 2
+  },
+  method: 'get'
+})
+
+axios.get('/extend/get', {
+  params: {
+    a: 1,
+    b: 2
+  }
+})
+
+axios.post('/extend/post')
+axios.options('/extend/options')
+axios.delete('/extend/delete')
+axios.head('/extend/head')
+axios.put('/extend/put')
+axios.patch('/extend/patch')
