cfc9d5af4785d2b031ca18aa50678a8a29d5f8cd
wangkaiwd <1162486176@qq.com>
2019-08-18 17:45:27 +0800

    feat: this stands for window when axios function is called

--- src/axios.ts
+++ src/axios.ts
@@ -6,6 +6,7 @@ import { extend } from './helpers/utils'
 // 实现方案： 创建一个class,class中有axios中多有要用到的属性，然后通过遍历将所有属性拷贝过来
 const createInstance = (): AxiosInstance => {
   const context = new Axios()
+  // instance及axios函数在调用的时候，this是window，所以如果有用到this的话，要进行更改this指向
   const instance = context.request
   return extend(instance, context)
 }
