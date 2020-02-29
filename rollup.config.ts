2f1775adc0a6fd9296b0ddf51f6e299bfbdfe302
wangkaiwd <1162486176@qq.com>
2020-02-29 14:29:42 +0800

    feat: temp clear all WebStorm errors

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,14 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+// @ts-ignore
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import { camelCase } from 'lodash';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
 
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
-const libraryName = 'typescript-project'
+const libraryName = 'typescript-project';
 
 export default {
   input: `src/${libraryName}.ts`,
@@ -35,4 +36,4 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-}
+};
