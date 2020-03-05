f825ac6e3a79cb6ca765fe5e7e81ca1c0b94ed1e
wangkaiwd <1162486176@qq.com>
2020-03-05 22:07:04 +0800

    feat: optimize axios response config content

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,13 +1,14 @@
-import { AxiosRequestConfig } from './types'
-
+import { AxiosRequestConfig, AxiosResponse } from './types'
+// FIXME: 该处代码逻辑较长，需要优化
 const xhr = (config: AxiosRequestConfig) => {
   return new Promise(resolve => {
     const { method = 'get', url, data = null, headers, responseType } = config
     const request = new XMLHttpRequest()
+    request.open(method.toUpperCase(), url)
     if (responseType) {
+      // 要在调用open()初始化请求和send()发送请求到服务器之前调用
       request.responseType = responseType
     }
-    request.open(method.toUpperCase(), url)
     Object.keys(headers).map(key => {
       // 如果没有传入data的话，会将Content-Type删除掉
       if (data === null && key.toLowerCase() === 'content-type') {
@@ -16,9 +17,24 @@ const xhr = (config: AxiosRequestConfig) => {
       request.setRequestHeader(key, headers[key])
     })
     request.send(data)
-    request.addEventListener('load', e => {
-      console.log('e', e.target)
-      resolve({})
+    request.addEventListener('readystatechange', e => {
+      // 这里也可以使用e.currentTarget来获取到当前的事件起源目标的引用(request)
+      // e.target: 触发事件的元素
+      // e.currentTarget: 附属于事件监听器的元素(绑定事件的元素)
+      if (request.readyState !== 4) {
+        return
+      }
+      const responseHeaders = request.getAllResponseHeaders()
+
+      const response: AxiosResponse = {
+        status: request.status,
+        statusText: request.statusText,
+        config,
+        request,
+        headers: responseHeaders,
+        data: request.response
+      }
+      resolve(response)
     })
   })
 }
