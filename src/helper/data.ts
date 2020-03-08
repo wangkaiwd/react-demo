fdc8ee51cc46d49a6939f52d52d57b30b4f26b12
wangkaiwd <1162486176@qq.com>
2020-03-08 17:36:02 +0800

    feat: axios base feature complement

--- src/helper/data.ts
+++ src/helper/data.ts
@@ -6,3 +6,14 @@ export const transformRequest = (data: any) => {
   }
   return data
 }
+
+// 由于不设置responseType的时候，会默认返回字符串类型的响应
+// 而我们想对json字符串在响应的时候进行自动转换
+export const transformResponse = (data: any) => {
+  if (typeof data !== 'string') return data
+  try {
+    return JSON.parse(data)
+  } catch (e) {
+    return data
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { AxiosPromise, AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildUrl } from './helper/url'
-import { transformRequest } from './helper/data'
+import { transformRequest, transformResponse } from './helper/data'
 import { processHeaders } from './helper/header'
 
 const axios = (config: AxiosRequestConfig): AxiosPromise => {
   // 发请求之前，先要对配置项进行处理
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(transformResponseData)
 }
 
 const processConfig = (config: AxiosRequestConfig) => {
   config.url = transformUrl(config)
   config.headers = transformHeaders(config)
-  config.data = transformData(config)
+  config.data = transformRequestData(config)
 }
 
 const transformUrl = (config: AxiosRequestConfig) => {
@@ -21,7 +21,7 @@ const transformUrl = (config: AxiosRequestConfig) => {
   return buildUrl(url, params)
 }
 
-const transformData = (config: AxiosRequestConfig) => {
+const transformRequestData = (config: AxiosRequestConfig) => {
   const { data } = config
   return transformRequest(data)
 }
@@ -29,6 +29,12 @@ const transformData = (config: AxiosRequestConfig) => {
 const transformHeaders = (config: AxiosRequestConfig) => {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+const transformResponseData = (response: AxiosResponse) => {
+  const { data } = response
+  response.data = transformResponse(data)
+  return response
 }
 
 export default axios
