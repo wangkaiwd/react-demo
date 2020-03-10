58fdb9417cfb08bfd0efef8933251951e8b51a4e
wangkaiwd <1162486176@qq.com>
2020-03-11 00:12:18 +0800

    feat: test all axios error properties

--- examples/error/app.ts
+++ examples/error/app.ts
@@ -51,6 +51,11 @@ axios({
   // 这里推断不出来err的类型: Promise中的错误处理函数的类型接受的参数为any
   //  onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null)
   // 这里我们手动指定err的类型
+  console.log(err.name)
+  console.log(err.code)
+  console.log(err.config)
+  console.log(err.message)
+  console.log(err.isAxiosError)
   console.log(err.request)
   console.log(err.response)
 })
